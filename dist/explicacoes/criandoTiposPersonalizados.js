9afee5026d8f11d2854e4f8eb4673b2fc3905f1d
João Victor <j.victorbahia@gmail.com>
2023-06-22 00:39:32 -0300

    estudo dia 22.06

---
+++ dist/explicacoes/criandoTiposPersonalizados.js
@@ -0,0 +1,13 @@
+// tipos literais, com eles podemos atribuir um valor especifico permitido para a variavel:
+let minhaVariavelLiteral; // essa variavel só poderá receber uma string "Olá"
+let outroExemplo; // essa variavel só poderá receber um number 5
+// por exemplo, uma "const" é considerada um literal pelo TS
+// posso usar essa barra para funcionar como um operador "ou" |
+let opção; // essa variavel só pode receber yes, no ou etc
+let exemplo; // vai poder receber valores do tipo number ou string
+//tenho então um conjunto de tipos que denominei de "Planet"
+let meusPlanetas; // agora minha variavel só pode receber esse conjunto de valores sem eu ter que repetir eles aqui
+let sistemaSolar;
+function planeta(planetas) {
+    //....
+}
